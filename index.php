71b6b87160db8b3c5a394422aacde90d13ddf31b
Ramon K <contato@ramonk.com>
2023-07-13 18:40:34 -0300

    feat(index.php): prepare `index.php` to deal with all archives

--- index.php
+++ index.php
@@ -1,6 +1,36 @@
 <?php get_header() ?>
 
 <div class="container">
+    <?php get_search_form(); ?>
+
+    <div class="bg-yellow-500 text-black">
+        <?php if (is_home()): ?>
+            <p>This is home.</p>
+        <?php elseif (is_search()): ?>
+            <p>This is search for
+                <?php echo get_search_query(); ?>.
+            </p>
+        <?php elseif (is_category()): ?>
+            <p>This is category
+                <?php single_cat_title(); ?>.
+            </p>
+            <?php if (!empty($cat_description = category_description())): ?>
+                <p>
+                    <?php echo $cat_description; ?>
+                </p>
+            <?php endif; ?>
+        <?php elseif (is_tag()): ?>
+            <p>This is tag
+                <?php single_tag_title(); ?>.
+            </p>
+            <?php if (!empty($tag_description = tag_description())): ?>
+                <p>
+                    <?php echo $tag_description; ?>
+                </p>
+            <?php endif; ?>
+        <?php endif ?>
+    </div>
+
     <?php if (have_posts()):
         while (have_posts()):
             the_post(); ?>
