c37b56e4214b5d053d4e1bb629ecf25456950ede
Ramon K <contato@ramonk.com>
2023-07-18 14:48:01 -0300

    wip

--- template-parts/header.php
+++ template-parts/header.php
@@ -1,12 +1,20 @@
-<div class="flex items-center justify-between space-x-2 py-2 px-4 level-1">
-    <div>
-
-        <h1 class="text-2xl font-bold">
-            <a href="<?php echo home_url(); ?>">
-                <?php bloginfo('name'); ?>
-            </a>
-        </h1>
-        <span>
+<div class="flex items-center justify-between py-[1rem] px-4 level-1">
+    <div class="flex flex-col">
+        <a href="<?php echo home_url(); ?>" class="leading-none">
+            <?php
+            $custom_logo_id = get_theme_mod('custom_logo');
+            $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+            if ($logo):
+                ?>
+                <img src="<?php echo $logo[0]; ?>" alt="<?php bloginfo('name'); ?>"
+                    class="inline-block h-[1.5rem] w-auto" />
+            <?php else: ?>
+                <span class="text-2xl font-sans font-bold leading-none">
+                    <?php bloginfo('name'); ?>
+                </span>
+            <?php endif; ?>
+        </a>
+        <span class="font-sans mt-1 leading-none text-xs hidden lg:block">
             <?php bloginfo('description'); ?>
         </span>
     </div>
@@ -15,7 +23,8 @@
         wp_nav_menu(
             array(
                 'container' => 'nav',
-                'menu_class' => 'flex justify-between space-x-2 font-sans font-normal',
+                'container_class' => 'fixed left-0 top-0 w-full h-full flex lg:static level-1 lg:bg-transparent opacity-95 lg:opacity-100 z-10',
+                'menu_class' => 'font-sans lg:flex lg:justify-between lg:space-x-4',
                 'theme_location' => 'main_menu'
             )
         );
