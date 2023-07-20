dbd2417b654dd2f503b0e977c338002859c038c7
Ramon K <contato@ramonk.com>
2023-07-20 20:54:22 -0300

    refactor(index.php): restyle format icons

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php get_header() ?>
-<div
-    class="max-w-[58rem] mx-auto py-[4rem] px-[1rem] border-b border-dotted border-stone-500 dark:border-stone-500 text-center">
+<div class="max-w-[58rem] mx-auto mt-0 py-[4rem] px-[1rem] text-center">
     <?php if (is_home()): ?>
         <h1 class="mb-[.25em]">
             <?php echo bloginfo('title') ?>
@@ -47,11 +46,13 @@
     while (have_posts()):
         the_post();
         $format = get_post_format() ? get_post_format() : 'standard'; ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class('relative lg:mx-auto max-w-[58rem] my-[6rem] group post-abstract'); ?>>
-            <span
-                class="inline-block mb-[1rem] ml-4 md:absolute md:opacity-75 group-hover:opacity-100 group-hover:text-stone-700 group-hover:border-stone-700 dark:group-hover:text-stone-300 dark:group-hover:border-stone-300 dark:opacity-100 hover:opacity-100 transition-all duration-200 p-[1rem] rounded rouded-full border md:border-stone-400 border-stone-400 hover:border-stone-700 text-stone-400 md:text-stone-400 dark:border-stone-500 dark:text-stone-500 md:dark:border-stone-500 md:dark:text-stone-500 md:dark:hover:border-stone-300 md:dark:hover:text-stone-300 top-0 mt-[.5rem]">
-                <?php echo zenc_get_format_icon($format, '1.5rem'); ?>
-            </span>
+        <article id="post-<?php the_ID(); ?>" <?php post_class('relative lg:mx-auto max-w-[58rem] pb-[6rem] post-abstract'); ?>>
+            <div class="border-t border-dotted border-stone-400 dark:border-stone-500 mb-[2.5rem]">
+                <span
+                    class="inline-block -mt-[1px] md:-ml-[3px] md:opacity-75 dark:opacity-100 transition-all duration-200 p-[1rem] rounded rouded-full border border-dotted border-stone-400 text-stone-400 dark:border-stone-500 dark:text-stone-500">
+                    <?php echo zenc_get_format_icon($format, '1.5rem'); ?>
+                </span>
+            </div>
             <div class="container">
                 <div class="mb-[1rem]">
                     <?php get_template_part('template-parts/post/meta', 'categories'); ?>
@@ -63,7 +64,7 @@
                 <div><!-- .container -->
         </article>
     <?php endwhile; ?>
-    <nav class="max-w-[58rem] mx-auto py-[4rem] border-t border-dotted border-stone-500 dark:border-stone-500">
+    <nav class="max-w-[58rem] mx-auto py-[4rem] border-t border-dotted border-stone-400 dark:border-stone-500">
     <?php get_template_part('template-parts/pagination', '', array('context' => 'loop')); ?>
     </nav>
 <?php else: ?>
