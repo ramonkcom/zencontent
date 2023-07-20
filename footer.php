14eec49bb6aedfa56ea9c0eca24598bbe9d2b676
Ramon K <contato@ramonk.com>
2023-07-20 11:48:25 -0300

    feat(footer.php): fix shadow

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 </main> <!-- #content -->
 
-<footer class="level-1 shadow-md flex justify-between py-[1rem] px-4">
+<footer
+    class="level-1 flex justify-between py-[1rem] px-4 shadow-[0_-4px_6px_-1px_rgba(0,0,0,.1),0_-2px_4px_-2px_rgba(0,0,0,.1)]">
     <div class="font-sans italic text-stone-500 hover:text-stone-600 dark:text-stone-400 dark:hover:text-stone-300">
         <?php echo get_theme_mod('footer_line'); ?>
     </div>
