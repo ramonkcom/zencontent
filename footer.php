7093eebdc6615a522946ec42206a86dcef9627ad
Ramon K <contato@ramonk.com>
2023-07-20 13:54:34 -0300

    feat(footer.php): add scroll to top button

--- footer.php
+++ footer.php
@@ -13,6 +13,12 @@
         </a>
     </div>
 </footer>
+<button class="btn p-[.625rem] fixed bottom-[1rem] right-4 transition-opacity duration-200" style="opacity:0">
+    <svg xmlns="http://www.w3.org/2000/svg" width=".75rem" height=".75rem" fill="currentColor" viewBox="0 0 16 16">
+        <path fill-rule="evenodd"
+            d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z" />
+    </svg>
+</button>
 
 <?php wp_footer(); ?>
 </body>
