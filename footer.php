b4b989dfc16a7d559327371cefeba23d93c4fc2d
Ramon K <contato@ramonk.com>
2023-07-20 11:39:54 -0300

    feat(footer.php): add content

--- footer.php
+++ footer.php
@@ -1,11 +1,15 @@
 </main> <!-- #content -->
 
-<footer class="level-1 shadow-md py-2 px-4 flex justify-between">
-    <div>
+<footer class="level-1 shadow-md flex justify-between py-[1rem] px-4">
+    <div class="font-sans italic text-stone-500 hover:text-stone-600 dark:text-stone-400 dark:hover:text-stone-300">
+        <?php echo get_theme_mod('footer_line'); ?>
     </div>
     <div>
-    </div>
-    <div>
+        <a href="http://github.com/ramonkcom/zencontent/" target="_blank" title="Zencontent Theme on GitHub"
+            class="font-sans font-bold text-stone-500 hover:text-stone-600 dark:text-stone-400 dark:hover:text-stone-300">
+            Zencontent©
+            <?php echo date('Y'); ?>
+        </a>
     </div>
 </footer>
 
