5a04cfb43628048ab2b5111b1e1324b358d407e2
Ramon K <contato@ramonk.com>
2023-07-20 17:04:02 -0300

    fix(index.php): improve format icon style

--- index.php
+++ index.php
@@ -37,9 +37,9 @@
     while (have_posts()):
         the_post();
         $format = get_post_format() ? get_post_format() : 'standard'; ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class('relative lg:mx-auto max-w-[58rem] post-abstract my-[6rem]'); ?>>
+        <article id="post-<?php the_ID(); ?>" <?php post_class('relative lg:mx-auto max-w-[58rem] my-[6rem] group post-abstract'); ?>>
             <span
-                class="absolute opacity-75 dark:opacity-100 hover:opacity-100 transition-all p-[1rem] rounded rouded-full border border-stone-400 hover:border-stone-700 text-stone-400 hover:text-stone-700 dark:border-stone-600 dark:text-stone-600 dark:hover:border-stone-300 dark:hover:text-stone-300 top-0 mt-[.5rem]">
+                class="inline-block mb-[1rem] ml-4 md:absolute md:opacity-75 group-hover:opacity-100 group-hover:text-stone-700 group-hover:border-stone-700 dark:group-hover:text-stone-300 dark:group-hover:border-stone-300 dark:opacity-100 hover:opacity-100 transition-all duration-200 p-[1rem] rounded rouded-full border md:border-stone-400 border-stone-400 hover:border-stone-700 text-stone-400 md:text-stone-400 dark:border-stone-500 dark:text-stone-500 md:dark:border-stone-500 md:dark:text-stone-500 md:dark:hover:border-stone-300 md:dark:hover:text-stone-300 top-0 mt-[.5rem]">
                 <?php echo zenc_get_format_icon($format, '1.5rem'); ?>
             </span>
             <div class="container">
