1707e989b8d26815fda90dae42d413b06c7d44a1
Ramon K <contato@ramonk.com>
2023-07-21 19:38:46 -0300

    feat(image): style single and abstract

--- template-parts/post/abstract-image.php
+++ template-parts/post/abstract-image.php
@@ -1,1 +1,28 @@
-<?php get_template_part('template-parts/post/abstract', 'default'); ?>
+<div class="flex flex-col">
+    <header class="narrow mt-[1.5em] order-2">
+        <div class="mb-[1rem]">
+            <?php get_template_part('template-parts/post/meta', 'categories'); ?>
+        </div>
+        <h2>
+            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                <?php the_title(); ?>
+            </a>
+        </h2>
+    </header>
+
+    <main class="wide order-1">
+        <?php
+            if (has_post_thumbnail()) {
+                the_post_thumbnail($size = 'post-thumbnail', array('class' => 'w-full'));
+            }
+        ?>
+    </main>
+        
+    <aside class="container order-3">
+        <?php the_content(); ?>
+    </aside>
+
+    <footer class="narrow mt-[1rem] order-last">
+        <?php get_template_part('template-parts/post/meta', 'tags'); ?>
+    <footer>
+</div>
--- template-parts/post/single-image.php
+++ template-parts/post/single-image.php
@@ -1,1 +1,26 @@
-<?php get_template_part('template-parts/post/single', 'default'); ?>
+<div class="flex flex-col">
+    <header class="narrow mt-[1.5em] order-2">
+        <div class="mb-[1.5rem]">
+            <?php get_template_part('template-parts/post/meta', 'categories'); ?>
+        </div>
+        <h1>
+            <?php the_title(); ?>
+        </h1>
+    </header>
+
+    <main class="wide order-1">
+        <?php
+            if (has_post_thumbnail()) {
+                the_post_thumbnail($size = 'post-thumbnail', array('class' => 'w-full'));
+            }
+        ?>
+    </main>
+
+    <aside class="container order-3">
+        <?php the_content(); ?>
+    </aside>
+
+    <footer class="narrow mt-[1.5rem] order-last">
+        <?php get_template_part('template-parts/post/meta', 'tags'); ?>
+    <footer>
+</div>
