f114813996424bb22c20ddeaef799d8b3f4bccfc
Ramon K <contato@ramonk.com>
2023-07-21 11:13:15 -0300

    fix(abstract-default.php): add `title` attribute to anchor

--- template-parts/post/abstract-default.php
+++ template-parts/post/abstract-default.php
@@ -1,6 +1,6 @@
 <header>
     <h2>
-        <a href="<?php the_permalink(); ?>">
+        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
             <?php the_title(); ?>
         </a>
     </h2>
