a46d9524b1f9368b0ee513a968a9e8cf016a39f4
Ramon K <contato@ramonk.com>
2023-07-21 11:46:32 -0300

    feat(status): style abstract and single

--- template-parts/post/abstract-status.php
+++ template-parts/post/abstract-status.php
@@ -1,1 +1,17 @@
-<?php get_template_part('template-parts/post/abstract', 'default'); ?>
+<header>
+    <h2 class="specific font-sans font-bold text-base">
+        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+            <?php
+            $title = get_the_title();
+            if (preg_match('/[a-zA-Z0-9]$/', $title)) {
+                $title .= ':';
+            }
+            echo $title;
+            ?>
+        </a>
+    </h2>
+</header>
+
+<main class="-mt-[1rem] text-2xl leading-snug">
+    <?php the_content(); ?>
+</main>
--- template-parts/post/single-status.php
+++ template-parts/post/single-status.php
@@ -1,1 +1,15 @@
-<?php get_template_part('template-parts/post/single', 'default'); ?>
+<header>
+    <h1 class="specific font-sans font-bold text-base">
+        <?php
+        $title = get_the_title();
+        if (preg_match('/[a-zA-Z0-9]$/', $title)) {
+            $title .= ':';
+        }
+        echo $title;
+        ?>
+    </h1>
+</header>
+
+<main class="-mt-[1rem] text-2xl leading-snug">
+    <?php the_content(); ?>
+</main>
