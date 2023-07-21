4f3f7ffc2ce69222372961a0bd134f44ad91cbad
Ramon K <contato@ramonk.com>
2023-07-21 11:18:06 -0300

    feat(gallery): style abstract and single

--- template-parts/post/abstract-gallery.php
+++ template-parts/post/abstract-gallery.php
@@ -1,1 +1,21 @@
-<?php get_template_part('template-parts/post/abstract', 'default'); ?>
+<?php
+$post_content = get_the_content();
+$gallery_block_pattern = '/<!--\s*wp:gallery\b(.*?)\s*-->.*?<!--\s*\/wp:gallery\s*-->/s';
+preg_match($gallery_block_pattern, $post_content, $matches);
+$post_content = preg_replace($gallery_block_pattern, '', $post_content);
+$gallery_block = empty($matches) ? null : $matches[0];
+?>
+
+<?php if ($gallery_block): ?>
+    <?php echo apply_filters('the_content', $gallery_block); ?>
+<?php endif; ?>
+<header class="mt-[1.5em]">
+    <h2>
+        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+            <?php the_title(); ?>
+        </a>
+    </h2>
+</header>
+<main>
+    <?php echo $post_content; ?>
+</main>
