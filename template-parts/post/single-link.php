85a4e11fef702f19b2dde6fb41135e66c3d8d56a
Ramon K <contato@ramonk.com>
2023-07-22 20:35:47 -0300

    feat(link): style single and abstract

--- template-parts/post/single-link.php
+++ template-parts/post/single-link.php
@@ -1,1 +1,16 @@
-<?php get_template_part('template-parts/post/single', 'default'); ?>
+<header class="narrow">
+    <div class="mb-[1.5em]">
+        <?php get_template_part('template-parts/post/meta', 'categories'); ?>
+    </div>
+    <h1>
+        <?php the_title(); ?>
+    </h1>
+</header>
+
+<main class="container">
+    <?php the_content(); ?>
+</main>
+
+<footer class="narrow mt-[1.5em]">
+    <?php get_template_part('template-parts/post/meta', 'tags'); ?>
+</footer>
