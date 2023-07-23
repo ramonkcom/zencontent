e2040533176d04192b81e5c7e6acce44490f1175
Ramon K <contato@ramonk.com>
2023-07-23 12:11:01 -0300

    feat(quote): style single and abstract

--- template-parts/post/abstract-quote.php
+++ template-parts/post/abstract-quote.php
@@ -1,1 +1,18 @@
-<?php get_template_part('template-parts/post/abstract', 'default'); ?>
+<header class="narrow">
+    <div class="mb-[1rem]">
+        <?php get_template_part('template-parts/post/meta', 'categories'); ?>
+    </div>
+    <h2 class="specific font-sans font-bold text-base text-stone-500 dark:text-stone-500 hidden">
+        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+            <?php the_title(); ?>
+        </a>
+    </h2>
+</header>
+
+<main class="container my-[1rem]">
+    <?php the_content(); ?>
+</main>
+
+<footer class="narrow mt-[1rem]">
+    <?php get_template_part('template-parts/post/meta', 'tags'); ?>
+<footer>
--- template-parts/post/single-quote.php
+++ template-parts/post/single-quote.php
@@ -1,1 +1,16 @@
-<?php get_template_part('template-parts/post/single', 'default'); ?>
+<header class="narrow">
+    <div class="mb-[1rem]">
+        <?php get_template_part('template-parts/post/meta', 'categories'); ?>
+    </div>
+    <h1 class="specific font-sans font-bold text-base text-stone-500 dark:text-stone-500 hidden">
+        <?php the_title(); ?>
+    </h1>
+</header>
+
+<main class="container my-[1rem]">
+    <?php the_content(); ?>
+</main>
+
+<footer class="narrow mt-[1rem]">
+    <?php get_template_part('template-parts/post/meta', 'tags'); ?>
+<footer>
